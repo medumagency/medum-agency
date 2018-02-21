0ee33b31014462adcab468984d46415afa3687de
stychu <tomaszstysial@o2.pl>
2018-02-21 22:24:26 +0100

    login form, job panel, visuals

--- src/app/components/cooperation-form/cooperation-form.component.ts
+++ src/app/components/cooperation-form/cooperation-form.component.ts
@@ -1,15 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FirestoreDaoService } from '../../services/dao/firestore-dao.service';
-import { Observable } from 'rxjs/Observable';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import 'rxjs/add/operator/map';
-
-interface IJobOffer {
-  title: string;
-  description: string;
-  date: number;
-  id?: string;
-}
 
 @Component({
   selector: 'app-cooperation-form',
@@ -18,13 +7,10 @@ interface IJobOffer {
 })
 export class CooperationFormComponent implements OnInit {
 
-  posts: Observable<IJobOffer[]>;
-
-  constructor(private firestoreDAO: FirestoreDaoService) {
+  constructor() {
   }
 
   ngOnInit() {
-    this.posts = this.firestoreDAO.getJobOffers();
   }
 
 }
--- src/app/components/country-job-offers/country-job-offers.component.ts
+++ src/app/components/country-job-offers/country-job-offers.component.ts
@@ -30,6 +30,27 @@ export class CountryJobOffersComponent implements OnInit {
   public optionsSmall = CountryJobOffersComponent.setOptions(325);
   public optionsMedium = CountryJobOffersComponent.setOptions(380);
   public optionsBig = CountryJobOffersComponent.setOptions();
+  public text = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque possimus, quidem! Ad at corporis dolore
+          facere laborum libero magnam optio quas quidem quod repellat, sapiente sed vero vitae voluptatem. Culpa cum
+          delectus dolor ea expedita, explicabo, id mollitia neque nesciunt obcaecati officiis perferendis porro
+          reprehenderit suscipit temporibus ut voluptatem. Dolore, dolorem eveniet inventore laborum modi nam quis
+          reprehenderit sequi! Aspernatur id natus tempora veniam veritatis! Cum dolore et expedita fugit, ipsa iure
+          neque non perferendis placeat, sequi tenetur ut voluptate? A consequuntur dolore eius enim et harum hic,
+          itaque minima mollitia nesciunt obcaecati qui repellat sapiente sunt tempore ut voluptatem. Assumenda beatae,
+          commodi cumque cupiditate deserunt dolor ea earum eius enim eum facere hic illum inventore ipsum labore,
+          laborum magnam maiores modi natus nihil numquam obcaecati praesentium quae quaerat quam quis reiciendis rem
+          repellat reprehenderit sequi tempore vel voluptate voluptatem. Aut delectus fuga quis repudiandae tempora.
+          Eaque error esse exercitationem expedita odit quam vitae voluptatibus. Ab aliquid assumenda cum odit quasi,
+          quidem repellat vel veniam. Alias aliquam, culpa iure iusto, minus molestias neque nesciunt optio quaerat
+          quibusdam recusandae, repudiandae. Amet assumenda autem, consequatur error nam natus quo. Adipisci asperiores
+          atque eius illum rem. Amet asperiores beatae delectus deleniti deserunt dolor dolore doloremque dolores eius
+          fugiat incidunt iste magnam nemo odio officiis pariatur quas quasi, quis reiciendis repellat rerum saepe
+          sapiente similique sit temporibus totam vel veniam. Aliquam exercitationem nobis recusandae sit suscipit,
+          voluptas. At beatae dolore eius fugiat molestiae officia, repudiandae velit. Accusantium aliquam, cum dolores
+          et eum explicabo fugiat maiores nihil odit quidem quisquam sapiente sit! A amet asperiores assumenda
+          consequuntur culpa cum, cupiditate dicta dolor dolore expedita facere fuga id illum impedit inventore ipsum
+          maiores minus modi natus nulla possimus praesentium, quae quam quibusdam quidem quis quo ratione similique
+          sint temporibus tenetur ut vitae voluptatibus! Aspernatur, ea, iusto!`;
 
   static setOptions(height = 480) {
     return {
@@ -51,6 +72,11 @@ export class CountryJobOffersComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  turnCate(str, length) {
+    return str.length > length ? `${str.slice(0, length)}...` : str;
+  }
+
 }
 
 // PL-LB	Lubuskie	Lubusz
