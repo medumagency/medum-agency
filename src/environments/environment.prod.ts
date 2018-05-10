9360cac87c878820c5a18d21e9daffaf6bc1c5ab
stychu <tomaszstysial@o2.pl>
2018-05-10 15:48:14 +0200

    maciek changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -15,7 +15,7 @@ export const environment = {
     apiKey: '5616637fd7e23889b5849bd7c66a6a57',
     appId: '6AD0897I3Y',
     indexName: 'jobOffers',
-    routing: true
+    urlSync: true
   },
   countryNames: 'https://gist.githubusercontent.com/stychu/f79a4240d36d29ff486eff854166f866/raw/7a6d616894908ca1fb9ce2d8bc32983a4cb600da/countries.json',
   polandCountry: 'https://gist.githubusercontent.com/stychu/4838cf6e3e094234a279d445714d141d/raw/aa5f941b0d6aae1358f9d64a23a1c862d907383f/polandRegionsCities.json'
